7dd9ecf34385e5f82a6fb6dab2c67150ce303190
Asus <rozzoqovshahzod@gmail.com>
2024-05-01 20:39:23 +0500

    Create repository

---
+++ src/main/java/uz/shoh/greenbazar/GreenBazarApplication.java
@@ -0,0 +1,12 @@
+package uz.shoh.greenbazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenBazarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreenBazarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/shoh/greenbazar/GreenBazarApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.shoh.greenbazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenBazarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
