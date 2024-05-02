4ff2d96636c9c8556c693a2aa019408587220338
Asus <rozzoqovshahzod@gmail.com>
2024-05-02 11:45:51 +0500

    Create repository

--- src/main/java/uz/shoh/greenbazar/GreenBazarApplication.java
+++ src/main/java/uz/shoh/greenbazar/GreenBazarApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GreenBazarApplication {
     public static void main(String[] args) {
         SpringApplication.run(GreenBazarApplication.class, args);
+
     }
 
 }
